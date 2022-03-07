50aa690a7d838632600c9840ee13d43bbb354544
Daniel Dansie <dan.dansie126@gmail.com>
2022-03-06 20:05:57 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,78 @@
+const storeItems = [
+    {
+        name: 'TV',
+        price: 800.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Phone',
+        price: 700.00,
+        inStock: false,
+        details: '5G'
+    },
+    {
+        name: 'Game Console',
+        price: 300.00,
+        inStock: true,
+        details: 'The latest and greatest'
+    },
+    {
+        name: 'Laptop',
+        price: 1200.00,
+        inStock: true,
+        details: '16GB RAM 1TB SSD'
+    },
+    {
+        name: 'Smart Watch',
+        price: 200.00,
+        inStock: false,
+        details: 'Counts your steps'
+    },
+    {
+        name: 'Headphones',
+        price: 100.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Keyboard',
+        price: 100.00,
+        inStock: true,
+        details: 'Types for you'
+    },
+    {
+        name: 'HDMI Cord',
+        price: 100.00,
+        inStock: true,
+        details: 'HDMI to USB type C'
+    },
+    {
+        name: 'Monitor',
+        price: 300.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Speaker',
+        price: 200.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Video Game',
+        price: 60.00,
+        inStock: true,
+        details: 'Enjoy for hours'
+    },
+];
+
+$.each(storeItems, function(index, value){
+    if (value.inStock) {
+      $("#productList").append("<p class='inStock'><span>$" + value.price + "</span><span> " + value.name + "</span> <span>" + value.details + "</span></p>");
+    }
+});
+
+$('#clickMe').click(function() {
+   $('#contentContainer').toggleClass('darkMode');
+})
